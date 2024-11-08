6354865c3e7dc3392a5cfbd61b336caec2171951
Asifa <princesherazkhan@gmail.com>
2024-11-09 01:00:24 +0500

    git push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import world from "@/components/world.jpeg"
+
 export default function Home(){
   return(
     <div className="parentContainer">
       <div className="childContainer">
-        
+
         Hello World!
 
       </div>
